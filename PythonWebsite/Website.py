639d8d2230d3075f5ef5c8bae1e8b88052e726e7
Jay-271 <116125775+Jay-271@users.noreply.github.com>
2022-11-11 14:55:48 -0500

    Update Website.py
    learning the basics of py

---
+++ PythonWebsite/Website.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+fruits = ["potato", "orange", "donut"]
+x, y, z = fruits
+print (x, y, z)
+
+def fruitFunc():
+    x = "fantastic"
+    print ("Python is " + x)
+
+fruitFunc()
+
+print("Python is " + x)
+
